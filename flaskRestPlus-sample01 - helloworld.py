8f0a79b4477310ef3b7e5a4146e98f2253a12a31
Marc Charmois <marc.charmois@gmail.com>
2018-01-20 14:25:29 +0100

    changing file and folder names

--- flaskRestPlus-sample01 - helloworld.py
+++ flaskRestPlus-sample01 - helloworld.py
@@ -4,10 +4,10 @@ from flask_restplus import Resource, Api
 app = Flask(__name__)                  #  Create a Flask WSGI appliction
 api = Api(app)                         #  Create a Flask-RESTPlus API
 
-@api.route('/hello')                   #  Create a URL route to this resource
+@api.route('/token')                   #  Create a URL route to this resource
 class HelloWorld(Resource):            #  Create a RESTful resource
     def get(self):                     #  Create GET endpoint
-        return {'hello': 'world'}
+        return {'token': 'gerg51-1s51fv-561dvsvd-fvkiy1d15d'}
 
 if __name__ == '__main__':
     app.run(debug=True)                #  Start a development server
